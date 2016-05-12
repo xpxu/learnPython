79ac6c61b084ac6e932ed1f4d0fc99cb5740812f
xp_xu <xp.xu@oracle.com>
2016-05-12 02:08:11 -0700

    add lz4-containter

--- jsondump/jsondump2file.py
+++ jsondump/jsondump2file.py
@@ -5,4 +5,4 @@ with open("test.json") as infile:
 j["Account"]["Containers"].append({"name": "mnop", "created_at": "100000.12345"})
 
 with open("test.json", "w") as outfile:
-    json.dump(j, outfile, indent=4)
+    json.dump(j, outfile, indent=4, separators=(',', ': '))
